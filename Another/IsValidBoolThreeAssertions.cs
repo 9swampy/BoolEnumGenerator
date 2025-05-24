4929656178f18522cb80bea337d6f8f0fdd00cd7
9swampy <github@prims.com>
2025-05-24 21:46:38 +0100

    So it was the generate namespace difference was the real problem...

--- Another/IsValidBoolThreeAssertions.cs
+++ Another/IsValidBoolThreeAssertions.cs
@@ -1,18 +1,18 @@
 ﻿
-namespace Another;
+//namespace Another;
 
-using FluentAssertions.Primitives;
-using FluentAssertions;
+//using FluentAssertions.Primitives;
+//using FluentAssertions;
 
-public partial class IsValidBoolThreeAssertions(IsValidBoolThree instance) : BooleanAssertions<IsValidBoolThreeAssertions>(instance)
-{
-  public AndConstraint<IsValidBoolThreeAssertions> NotBe(IsValidBoolThree unexpected, string because = "", params object[] becauseArgs)
-  {
-    return base.NotBe(unexpected.Value, because, becauseArgs);
-  }
+//public partial class IsValidBoolThreeAssertions(IsValidBoolThree instance) : BooleanAssertions<IsValidBoolThreeAssertions>(instance)
+//{
+//  public AndConstraint<IsValidBoolThreeAssertions> NotBe(IsValidBoolThree unexpected, string because = "", params object[] becauseArgs)
+//  {
+//    return base.NotBe(unexpected.Value, because, becauseArgs);
+//  }
 
-  public AndConstraint<IsValidBoolThreeAssertions> Be(IsValidBoolThree expected, string because = "", params object[] becauseArgs)
-  {
-    return base.Be(expected.Value, because, becauseArgs);
-  }
-}
+//  public AndConstraint<IsValidBoolThreeAssertions> Be(IsValidBoolThree expected, string because = "", params object[] becauseArgs)
+//  {
+//    return base.Be(expected.Value, because, becauseArgs);
+//  }
+//}
